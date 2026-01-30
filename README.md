ca49148fcee6e227a38f720097b60d66a41f6e0b
Koti9013 <artemen000@gmail.com>
2026-01-30 20:10:02 +0200

    Release

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple-antivirus
+This simple project can scan your task manager &amp; startup files, can find intruders and report them to you. It is also open source, so it guarantees you that it will not harm your computer.
+ # How to use
+ It's an often question to developers. The project is really easy in use, so thats how you start it:
+ 1. First, you must start it with administrator rights. Don't worry, the script need it only to work correct and to be able to deactivate the intruders' instruments that can really harm your PC.
+ 2. Then, wait for the script to start. It will automatically check the latest updates when you launch it.
+ 3. When the antivirus find some suspicious files it will automatically report you about it. Then it's up to you.
+ 4. The script will check autostarting tasks and read all about their path. If the task's home is a folder for short-timed files, then it will tell you about it. Don't worry, the system's files are ignored. It will also ignore such files as "OneDrive" etc., so it will not harm your PC
+ 5. You can tell it to delete it or skip the file and continue scanning. It's easy to use, I tried to do everything to optimizate it for you :)
+# [Found some issues?](https://github.com/Koti9013/Simple-antivirus/issues)
+You can tell me about it, just click the text up above and explain the issue, even offer me some new features!
+
+## License and Usage
+This project is **Source-Available**. The code is public to ensure 
+transparency and security, allowing anyone to audit the software. 
+
+**However, copying, modification, or redistribution of this code is strictly prohibited.** 
+All rights are reserved by the author.
---
+++ Simple_antivirus.ps1
@@ -0,0 +1,80 @@
+﻿[Console]::OutputEncoding = [System.text.Encoding]::UTF8
+$Host.UI.RawUI.WindowTitle = "Simple antivirus - v1.0"
+
+$localPath = $PSCommandPath
+$tempPath  = "$env:TEMP\update_temp.ps1"
+
+Write-Host "--- Cheching for updates ---" -ForegroundColor Cyan
+
+$filesToUpdate = @("Simple antivirus.ps1", "README.md", "LICENSE.md")
+$repoUrl = 
+
+foreach ($fileName in $filesToUpdate) {
+    $localPath = Join-Path $PSScriptRoot $fileName
+    $tempPath = "$localPath.tmp"
+    $remoteUrl = #Add
+
+    try {
+        Invoke-WebRequest -Uri $remoteUrl -OutFile $tempPath -ErrorAction Stop -TimeoutSec 10
+        
+        $localHash = if (Test-Path $localPath) { (Get-FileHash $localPath).Hash } else { "" }
+        $remoteHash = (Get-FileHash $tempPath).Hash
+
+        if ($localHash -ne $remoteHash) {
+            Write-Host "[!] Updating: $fileName" -ForegroundColor Yellow
+            Move-Item -Path $tempPath -Destination $localPath -Force
+        } else {
+            Write-Host "[✓] $fileName is up to date." -ForegroundColor Green
+            Remove-Item $tempPath
+        }
+    } catch {
+        Write-Host "[!] Could not update $fileName (No connection)" -ForegroundColor Gray
+        if (Test-Path $tempPath) { Remove-Item $tempPath }
+    }
+}
+
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Asking for administrator rights..." -ForegroundColor Yellow
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+
+
+Write-Host "==================================================="
+Write-Host "             Scanning the system...                "
+Write-Host "==================================================="
+
+$tasks = Get-ScheduledTask | Where-Object { $_.State -ne 'Disabled' }
+$found = $false
+
+
+foreach ($task in $tasks) {
+    $allActions = @()
+    foreach ($act in $Actions) {
+        if ($act.Execute) { $allActions += $act.Execute }
+        if ($act.Arguments) { $allActions += $act.Arguments }
+}
+$actionString = $allActions -join  " "
+
+if ($actionString -match 'Appdata|Temp' -and $actionString -notmatch 'OneDrive|Discord|Steam|Zoom') {
+   $found = $true
+   Write-Host "[!] INTRUDER FOUND!" -ForegroundColor Red
+   Write-Host "Task: $($task.TaskName)" -ForegroundColor Yellow
+   Write-Host "Path: $actionString" -ForegroundColor White
+
+
+   $choice = Read-Host "Disable this task? (y/n)"
+   if ($choice -eq 'y') {
+       Disable-ScheduledTask -TaskName $task.TaskName
+       Write-Host "Succesfully disabled!" -ForegroundColor Cyan
+   } else {
+       Write-Host "Task remains enabled." -ForegroundColor Gray
+   }
+   Write-Host "---------------------------"
+  }
+ } 
+ if (-not $found) {Write-Host "No threats found."-ForegroundColor Green}
+ Write-Host "Scan complete"
+ pause
+ exit
